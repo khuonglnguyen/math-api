f8107c4ad5a82e406b028d50c35c57849356003a
Khuong L. Nguyen <lapankhuongnguyen@gmail.com>
2023-11-18 19:35:41 +0700

    first commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import request, jsonify
+
+@app.route('/add', methods=['GET'])
+def add():
+    a = request.args.get('a', type=int)
+    b = request.args.get('b', type=int)
+
+    if a is not None and b is not None:
+        result = a + b
+        return jsonify({'result': result})
+    else:
+        return jsonify({'error': 'Invalid input'}), 400
+    
