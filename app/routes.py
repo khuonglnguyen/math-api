4a98666a49c833078617959f478c7dbe0dd5d605
Karshoul <Khuongmingo@gmail.com>
2023-11-18 20:22:50 +0700

    #1 add sub numbers API

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,13 @@ def add():
     else:
         return jsonify({'error': 'Invalid input'}), 400
     
+@app.route('/sub', methods=['GET'])
+def sub():
+    a = request.args.get('a', type=int)
+    b = request.args.get('b', type=int)
+
+    if a is not None and b is not None:
+        result = a - b
+        return jsonify({'result': result})
+    else:
+        return jsonify({'error': 'Invalid input'}), 400
